a2991869929f397cd224bbf8b1177b6633c3d28a
Oliver Summerell <146749730+OllieS-PhD@users.noreply.github.com>
2025-07-17 16:00:21 +0100

    Update runner.bat

--- runner.bat
+++ runner.bat
@@ -1,13 +1,12 @@
 @echo off
-set mod1=
-set mod2=
-set mod3="MLP"
-set mod="PointNet" "GUNet" "GraphSAGE"
+set mod= "MLP" "PointNet" "GUNet" "GraphSAGE"
 set task="-t full"
-set foils= 55 150 400
+set foils= 5 20 55 150
 set epochs=50 100 200 400
 
 for %%m in (%mod%) do (
-python C:\Users\olive\Documents\Code\eXFoil\eX-Foil\main.py %%m %task% -f 400
+for %%f in (%foils%) do (
+python C:\Users\olive\Documents\Code\eXFoil\eX-Foil\main.py %%m -f %%f -e 400
+)
 )
 python C:\Users\olive\Documents\Code\eXFoil\eX-Foil\validation.py
